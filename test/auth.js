d60aefdc2fad042fbefe351f7859c7417d1387a2
Derek Collison <derek.collison@gmail.com>
2011-12-30 16:09:08 -0600

    Added authorization

---
+++ test/auth.js
@@ -0,0 +1,46 @@
+var NATS = require ('../'),
+    nsc = require('./support/nats_server_control');
+
+var PORT = 1422;
+var flags = ['--user', 'derek', '--pass', 'foobar'];
+var authUrl = 'nats://derek:foobar@localhost:' + PORT;
+var noAuthUrl = 'nats://localhost:' + PORT;
+
+describe('Authorization', function() {
+
+  var server;
+
+  // Start up our own nats-server
+  before(function(done) {
+    server = nsc.start_server(PORT, flags, done);
+  });
+
+  // Shutdown our server after we are done
+  after(function(){
+    server.kill();
+  });
+
+  it('should fail to connect with no credentials ', function(done) {
+    var nc = NATS.connect(PORT);
+    nc.on('error', function(err) {
+      err.should.exist;
+      /Authorization/.exec(err).should.exist;
+      done();
+    });
+  });
+
+  it('should connect with proper credentials in url', function(done) {
+    var nc = NATS.connect(authUrl);
+    nc.on('connect', function(nc) {
+      setTimeout(done, 100);
+    });
+  });
+
+  it('should connect with proper credentials as options', function(done) {
+    var nc = NATS.connect({'url':noAuthUrl, 'user':'derek', 'pass':'foobar'});
+    nc.on('connect', function(nc) {
+      setTimeout(done, 100);
+    });
+  });
+
+});
