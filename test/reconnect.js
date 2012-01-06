0d03194ece0fcbe75b13736669236c1f4a710988
Derek Collison <derek.collison@gmail.com>
2012-01-06 06:43:31 -0600

    Reconnect logic updates

--- test/reconnect.js
+++ test/reconnect.js
@@ -4,6 +4,9 @@ var NATS = require ('../'),
 var PORT = 1421;
 var uri = 'nats://localhost:' + PORT;
 
+var WAIT = 20;
+var ATTEMPTS = 4;
+
 describe('Reconnect functionality', function() {
 
   var server;
@@ -18,24 +21,12 @@ describe('Reconnect functionality', function() {
     server.kill();
   });
 
-  it('should emit a reconnecting event', function(done) {
-    var nc = NATS.connect(PORT);
+  it('should not emit a reconnecting event if suppressed', function(done) {
+    var nc = NATS.connect({'port':PORT, 'reconnect':false});
     nc.should.exist;
-    nc.on('reconnecting', function(client) {
-      nc.close();
-      server = nsc.start_server(PORT, done);
-    });
-    nc.on('disconnected', function() {
-      done(new Error('Disconnect improperly called'));
-    });
-    nc.flush(function() {
+    nc.on('connect', function() {
       server.kill();
     });
-  });
-
-  it('should not emit a reconnecting event if suppressed', function(done) {
-    var nc = NATS.connect({ 'port' : PORT, 'reconnect' : false });
-    nc.should.exist;
     nc.on('reconnecting', function(client) {
       done(new Error('Reconnecting improperly called'));
     });
@@ -43,8 +34,45 @@ describe('Reconnect functionality', function() {
       nc.close();
       server = nsc.start_server(PORT, done);
     });
-    nc.flush(function() {
+  });
+
+  it('should emit a reconnecting event after proper delay', function(done) {
+    var nc = NATS.connect({'port':PORT, 'reconnectTimeWait':WAIT});
+    var startTime;
+    nc.should.exist;
+    nc.on('connect', function() {
       server.kill();
+      startTime = new Date();
+    });
+    nc.on('reconnecting', function(client) {
+      var elapsed = new Date() - startTime;
+      elapsed.should.be.within(WAIT, 2*WAIT);
+      nc.close();
+      server = nsc.start_server(PORT, done);
+    });
+    nc.on('disconnected', function() {
+      done(new Error('Disconnect improperly called'));
+    });
+  });
+
+  it('should emit multiple reconnecting events and fail after maxReconnectAttempts', function(done) {
+    var nc = NATS.connect({'port':PORT, 'reconnectTimeWait':WAIT, 'maxReconnectAttempts':ATTEMPTS});
+    var startTime;
+    var numAttempts = 0;
+    nc.on('connect', function() {
+      server.kill();
+      startTime = new Date();
+    });
+    nc.on('reconnecting', function(client) {
+      var elapsed = new Date() - startTime;
+      elapsed.should.be.within(WAIT, 2*WAIT);
+      startTime = new Date();
+      numAttempts += 1;
+    });
+    nc.on('disconnected', function() {
+      numAttempts.should.equal(ATTEMPTS);
+      nc.close();
+      done();
     });
   });
 
