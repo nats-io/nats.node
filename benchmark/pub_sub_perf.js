b6957055ff8fe2ba6c3f5de248c60f2f9c733fb4
Derek Collison <derek.collison@gmail.com>
2012-01-12 22:23:45 -0800

    Make sure sub is registered

--- benchmark/pub_sub_perf.js
+++ benchmark/pub_sub_perf.js
@@ -6,19 +6,19 @@ var nc2 = require('nats').connect();
 // Publish/Subscribe Performance
 ///////////////////////////////////////
 
-var loop = 100000;
-var hash = 2500;
+var loop = 50000;
+var hash = 1000;
 
 console.log('Publish/Subscribe Performance Test');
 
 nc1.on('connect', function() {
 
   var received = 0;
-
   var start = new Date();
 
   nc1.subscribe('test', function() {
     received += 1;
+
     if (received === loop) {
       var stop = new Date();
       var mps = parseInt(loop/((stop-start)/1000));
@@ -28,11 +28,14 @@ nc1.on('connect', function() {
     }
   });
 
-  for (var i=0; i<loop; i++) {
-    nc2.publish('test', 'ok');
-    if (i % hash === 0) {
-      process.stdout.write('+');
+  // Make sure sub is registered
+  nc1.flush(function() {
+    for (var i=0; i<loop; i++) {
+      nc2.publish('test', 'ok');
+      if (i % hash === 0) {
+	process.stdout.write('+');
+      }
     }
-  }
+  });
 
 });
