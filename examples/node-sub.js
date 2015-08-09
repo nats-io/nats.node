d6904890d866b9e8d541ccfd9e799d1a46396103
cmfatih <cmuratfatih@gmail.com>
2015-08-09 15:06:42 -0400

    Update for linting

--- examples/node-sub.js
+++ examples/node-sub.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 
+/* jslint node: true */
+'use strict';
+
 var nats = require ('nats').connect();
 
-nats.on('error', function(exception) {
-  console.log("Can't connect to the nats-server [" + nats.options.url + "] is it running?");
+nats.on('error', function(/*exception*/) {
+  console.log('Can\'t connect to the nats-server [' + nats.options.url + '] is it running?');
 });
 
 var subject = process.argv[2];
 
-if (subject == undefined) {
+if (!subject) {
   console.log('Usage: node-sub <subject>');
   process.exit();
 }
@@ -16,5 +19,5 @@ if (subject == undefined) {
 console.log('Listening on [' + subject + ']');
 
 nats.subscribe(subject, function(msg) {
-  console.log("Received '" + msg + "'");
+  console.log('Received "' + msg + '"');
 });
