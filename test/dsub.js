0a430abf9efff11e94bc0e054359559d5b16ecee
Alberto Ricart <alberto@synadia.com>
2020-08-07 11:05:48 -0500

    [FIX #367] tls.connect callback performed a flushPending which didn't strip subs. (#368)

--- test/dsub.js
+++ test/dsub.js
@@ -22,25 +22,33 @@ const after = require('mocha').after
 const before = require('mocha').before
 const describe = require('mocha').describe
 const it = require('mocha').it
+const should = require('should')
 
 describe('Double SUBS', () => {
   const PORT = 1922
-  const flags = ['-DV']
+  const TLSPORT = 2292
   let server
+  let tlsServer
 
   // Start up our own nats-server
   before(done => {
-    server = nsc.startServer(PORT, flags, done)
+    const flags = ['-DV', '--tls', '--tlscert', './test/certs/server.pem',
+      '--tlskey', './test/certs/key.pem']
+    server = nsc.startServer(PORT, ['-DV'], () => {
+      tlsServer = nsc.startServer(TLSPORT, flags, done)
+    })
   })
 
   // Shutdown our server after we are done
   after(done => {
-    nsc.stopServer(server, done)
+    nsc.stopServer(server, () => {
+      nsc.stopServer(tlsServer, done)
+    })
   })
 
   it('should not send multiple subscriptions on startup', done => {
     let subsSeen = 0
-    const subRe = /(\[SUB foo \d\])+/g
+    const subRe = /(\[SUB foo \d])+/g
 
     // Capture log output from nats-server and check for double SUB protos.
     server.stderr.on('data', data => {
@@ -54,9 +62,31 @@ describe('Double SUBS', () => {
     nc.on('connect', nc => {
       setTimeout(() => {
         nc.close()
-        subsSeen.should.equal(1)
+        should.equal(subsSeen, 1)
         done()
-      }, 100)
+      }, 400)
+    })
+  })
+
+  it('should not send multiple subscriptions on tls startup', done => {
+    let subsSeen = 0
+    const subRe = /(\[SUB foo \d])+/g
+
+    // Capture log output from nats-server and check for double SUB protos.
+    tlsServer.stderr.on('data', data => {
+      while (subRe.exec(data) !== null) {
+        subsSeen++
+      }
+    })
+
+    const nc = NATS.connect({ port: TLSPORT, tls: { rejectUnauthorized: false } })
+    nc.subscribe('foo')
+    nc.on('connect', nc => {
+      setTimeout(() => {
+        nc.close()
+        should.equal(subsSeen, 1)
+        done()
+      }, 400)
     })
   })
 })
