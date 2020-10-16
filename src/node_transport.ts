b44d5782391e0ab3b348333afb2118a6a60d2b83
Alberto Ricart <alberto@synadia.com>
2020-10-16 17:18:13 -0500

    upgrade nats-base-client

--- src/node_transport.ts
+++ src/node_transport.ts
@@ -36,7 +36,7 @@ import {
 const { resolve } = require("path");
 const { readFile, existsSync } = require("fs");
 
-const VERSION = "2.0.0-209";
+const VERSION = "2.0.0-210";
 const LANG = "nats.js";
 
 export class NodeTransport implements Transport {
