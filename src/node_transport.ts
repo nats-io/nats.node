0f730ff1c085c0a3ed811a0a1cd9f5076034351e
Alberto Ricart <alberto@synadia.com>
2021-03-26 19:39:27 -0500

    [update] updated nbc to 1.0.1 (#416)

--- src/node_transport.ts
+++ src/node_transport.ts
@@ -33,7 +33,7 @@ import { connect as tlsConnect, TlsOptions, TLSSocket } from "tls";
 const { resolve } = require("path");
 const { readFile, existsSync } = require("fs");
 
-const VERSION = "2.0.0";
+const VERSION = "2.0.1";
 const LANG = "nats.js";
 
 export class NodeTransport implements Transport {
