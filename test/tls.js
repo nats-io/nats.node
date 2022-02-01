cfd29076ff28630a2d4e3311a66e00df0a50d3eb
Alberto Ricart <alberto@synadia.com>
2022-02-01 16:52:05 -0600

    logging port for connection

--- test/tls.js
+++ test/tls.js
@@ -46,6 +46,7 @@ test("tls - fail if server doesn't support TLS", async (t) => {
       t.fail("shouldn't have connected");
     })
     .catch((err) => {
+      t.log(`failed trying to connect to localhost:${ns.port}`)
       t.is(err.code, ErrorCode.ServerOptionNotAvailable);
       lock.unlock();
     });
