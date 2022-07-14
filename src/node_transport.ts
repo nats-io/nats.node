038d87977b6aa5a1abaeefaf0a4220e2931b0449
Alberto Ricart <alberto@synadia.com>
2022-07-14 15:47:13 -0500

    [UPDATE] nkeys.js deps to 1.0.3 (#511)
    [UPDATE] dev dependencies

--- src/node_transport.ts
+++ src/node_transport.ts
@@ -34,7 +34,7 @@ const { resolve } = require("path");
 const { readFile, existsSync } = require("fs");
 const dns = require("dns");
 
-const VERSION = "2.7.1";
+const VERSION = "2.7.2-0";
 const LANG = "nats.js";
 
 export class NodeTransport implements Transport {
