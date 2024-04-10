83b1f6bc163ea66810a054e062fbc800636bdf67
Alberto Ricart <alberto@synadia.com>
2024-04-10 09:43:23 -0500

    [BUMP] nbc to v1.22.0, client version [CI] allow CI to publish npm package to latest

--- src/node_transport.ts
+++ src/node_transport.ts
@@ -34,7 +34,7 @@ const { resolve } = require("path");
 const { readFile, existsSync } = require("fs");
 const dns = require("dns");
 
-const VERSION = "2.21.0";
+const VERSION = "2.22.0";
 const LANG = "nats.js";
 
 export class NodeTransport implements Transport {
